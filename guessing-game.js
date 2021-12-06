2655f1ff5e747f2bfec996e534139ccaf378b858
Keon Jukes <keonofthephanerozoiceon@gmail.com>
2021-12-06 08:26:28 -0500

    Guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -1,25 +1,91 @@
 
-function getTitle(){
-    let title = document.getElementById('title');
+let startGame;
+//Constructor function which can be invoked with 'new' keyword to continously create new games
+function Game(){
+    this.playerGuess = null;
+    this.previousGuesses = [];
+    this.winningNumber = generateWinningNumber();
+}
 
-    title.innerHTML = '? THE GUESSING GAME ?';
-    // title.id = 'already-changed';
+function generateWinningNumber(){
+    return Math.floor(Math.random() * 101);
+}
+
+function newGame(){
+    return new Game();
 }
 
 
-let guessButton = document.getElementById('submit-guess')
+Game.prototype.playerGuessSubmission = function(guess){
+    if(isNaN(guess) || guess < 1 || guess > 100){
+        return 'that guess is invalid, enter a number between 1-100'
+    }
+    this.playerGuess = guess;
+    return this.guessCheck();
+}
 
+Game.prototype.guessCheck = function(){
+    if(this.previousGuesses.includes(this.playerGuess)){
+        return 'you\'ve already guessed that Number';
+    }
+    if(this.playerGuess === this.winningNumber){
+        return `You win! The number was ${this.winningNumber}`
+    } else if(this.playerGuess < this.winningNumber){
+        if(this.previousGuesses.length >= 5) {
+            return 'you lose';
+        } else{
+            this.previousGuesses.push(this.playerGuess)
+        return 'Guess higher'
+        }
+    } else if(this.playerGuess > this.winningNumber){
+        if(this.previousGuesses.length >= 5) {
+            return 'you lose';
+        } else {
+            this.previousGuesses.push(this.playerGuess)
+            return 'Guess lower';
+        }
+       
+    }
+}
+
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+let guessButton = document.getElementById('submit-guess')
+let playAgainButton = document.getElementById('play-again')
+let hintButton = document.getElementById('give-hint')
+let guess1 = document.getElementById('guess-1')
+let guess2 = document.getElementById('guess-2')
+let guess3 = document.getElementById('guess-3')
+let guess4 = document.getElementById('guess-4')
 
 function submittedGuess(inputElement){
-    const numGuess = inputElement.value;
-    inputElement.value = 0;
-    alert(numGuess);
+    const numGuess = Number(inputElement.value); //coercion to change String value to Number
+    alert(startGame.playerGuessSubmission(numGuess));
+    if(isNaN(numGuess) || numGuess < 1 || numGuess > 100){
+        console.log('invalid number, try again')
+    } else if(startGame.previousGuesses.length === 1){
+        guess1.innerHTML = numGuess;
+    } else if(startGame.previousGuesses.length === 2) {
+        guess2.innerHTML = numGuess;
+    } else if(startGame.previousGuesses.length === 3) {
+        guess3.innerHTML = numGuess;
+    } else if(startGame.previousGuesses.length === 4) {
+        guess4.innerHTML = numGuess;
+    } else{
+        alert('you have lost this game')
+        guess1.innerHTML = "";
+        guess2.innerHTML = "";
+        guess3.innerHTML = "";
+        guess4.innerHTML = "";
+        startGame = newGame();
+    }
+    inputElement.value = '';
 }
 
 
 guessButton.addEventListener('click', function(){
-    alert('This button was clicked');
-
+    
     const inputElement = document.querySelector('input')
 
     submittedGuess(inputElement)
@@ -27,15 +93,24 @@ guessButton.addEventListener('click', function(){
 
 
 
-let playAgainButton = document.getElementById('play-again')
 
 playAgainButton.addEventListener('click', function(){
-    alert('This button was clicked');
+    alert('starting a new game!');
+        guess1.innerHTML = "";
+        guess2.innerHTML = "";
+        guess3.innerHTML = "";
+        guess4.innerHTML = "";
+        startGame = newGame();
 });
 
-let hintButton = document.getElementById('give-hint')
+
 
 hintButton.addEventListener('click', function(){
     alert('you\'re very close');
+});
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    startGame = newGame();
 });
 
